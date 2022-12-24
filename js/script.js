cce4e2abc925f957193ff15ba5dc61d5e9689851
Andrey <stepanov.andrii.public@gmail.com>
2022-12-24 22:31:35 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+"use strict"
+
+
+// alert('Hello');
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18")
+// console.log(answer + 10);
+
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`)
+//
+// const user = 'Ivan';
+// alert(`Привет, ${user}`);
+    // let incr = 10,
+    //     decr = 10;
+
+// incr++;
+// decr--;
+// console.log(++incr);
+// console.log(--decr);
+//
+//
+// console.log(5%2);
+
+console.log(2+2*2 !== 8);
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose);
+
