b66283e99882728b2234476c5ee174f9f1e68459
Andrey <stepanov.andrii.public@gmail.com>
2022-12-25 19:34:46 +0200

    first home task

--- js/script.js
+++ js/script.js
@@ -1,43 +1,42 @@
 "use strict"
+/* Задание на урок:
 
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
 
-// alert('Hello');
-// const result = confirm("Are you here?");
-// console.log(result);
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
 
-// const answer = +prompt("Вам есть 18?", "18")
-// console.log(answer + 10);
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате:
+    movies: {
+        'logan': '8.1'
+    }
 
+Проверить, чтобы все работало без ошибок в консоли */
 
-// const answers = [];
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// answers[0] = prompt('Как ваше имя?', '');
-// answers[1] = prompt('Как ваша фамилия?', '');
-// answers[2] = prompt('Сколько вам лет?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
 
-// console.log(typeof(answers));
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-
-// const category = 'toys';
-// console.log(`https://someurl.com/${category}/5`)
-//
-// const user = 'Ivan';
-// alert(`Привет, ${user}`);
-    // let incr = 10,
-    //     decr = 10;
-
-// incr++;
-// decr--;
-// console.log(++incr);
-// console.log(--decr);
-//
-//
-// console.log(5%2);
-
-console.log(2+2*2 !== 8);
-
-const isChecked = false,
-      isClose = false;
-
-console.log(isChecked || !isClose);
-
+console.log(personalMovieDB);
